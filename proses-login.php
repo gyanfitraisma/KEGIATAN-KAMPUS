22780228f770e05f79244a62fa7531434b0cfac8
Ridhatul Jannati <jannatiridhatul39@gmail.com>
2026-07-08 08:19:50 +0700

    menambah proses login

---
+++ proses-login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include "koneksi.php";
+
+if(isset($_POST['username']) && isset($_POST['password'])){
+
+    $username = mysqli_real_escape_string($conn,$_POST['username']);
+    $password = mysqli_real_escape_string($conn,$_POST['password']);
+
+    $query = mysqli_query($conn,"SELECT * FROM user_login
+        WHERE username='$username'
+        AND password='$password'
+        AND status='aktif'");
+
+    if(mysqli_num_rows($query) > 0){
+
+        $data = mysqli_fetch_assoc($query);
+
+        $_SESSION['login'] = true;
+        $_SESSION['id_user'] = $data['id_user'];
+        $_SESSION['username'] = $data['username'];
+        $_SESSION['nama_lengkap'] = $data['nama_lengkap'];
+        $_SESSION['role'] = $data['role'];
+
+        header("Location: dashboard.php");
+        exit;
+
+    }else{
+
+        header("Location: index.php?error=1");
+        exit;
+
+    }
+
+}else{
+
+    header("Location: index.php");
+    exit;
+
+}
+?>
